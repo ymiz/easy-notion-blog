6e71c1f382cf7f6725b2ec1179124784cdeed20a
ymiz <ymizkm@gmail.com>
2023-01-03 10:48:04 +0900

    feat(page): 試しに数行消してみる

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,6 @@ const RootPage = () => (
     <div className={styles.container}>
       <div>
         <h2>Welcome!</h2>
-        <p>Your easy-notion-blog deployed successfully!</p>
-        <p>Have fun!</p>
         <p>
           easy-notion-blog powered by{' '}
           <Link href="https://github.com/otoyo/easy-notion-blog">
